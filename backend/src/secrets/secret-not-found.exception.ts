acf0681b898dd116c9d19af8b9216f0173a98092
Stijn Mommersteeg <stijnmommersteeg@gmail.com>
2026-08-19 23:23:11 +0200

    Forgot translation in exception

--- backend/src/secrets/secret-not-found.exception.ts
+++ backend/src/secrets/secret-not-found.exception.ts
@@ -2,6 +2,6 @@ import { NotFoundException } from '@nestjs/common';
 
 export class SecretNotFoundException extends NotFoundException {
   constructor() {
-    super('Deze link is ongeldig, verlopen of al bekeken.');
+    super('This link is invalid, expired or already viewed.');
   }
 }
